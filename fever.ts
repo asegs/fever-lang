14cca465659e940f5d11d39be3bb9a80099b3378
Aaron <aarsegal@gmail.com>
2024-06-11 13:46:33 -0400

    Migrated file.js to TS

--- fever.ts
+++ fever.ts
@@ -1,12 +1,12 @@
-//import { externalFile } from "./interactives/file.js";
 import { prompt } from "./interactives/repl.ts";
 import { ctx } from "./interpreter.ts";
+import { externalFile } from "./interactives/file.ts";
 
-// if (process.argv.length > 2) {
-//   for (let i = 2; i < process.argv.length; i++) {
-//     const name = process.argv[i];
-//     externalFile(name, ctx);
-//   }
-// }
+if (process.argv.length > 2) {
+  for (let i = 2; i < process.argv.length; i++) {
+    const name = process.argv[i];
+    externalFile(name);
+  }
+}
 
 prompt();
